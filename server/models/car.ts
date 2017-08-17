031c8e98c588d1e13be1760941556ab68e5b16a7
zhouzhou-96 <956544464@qq.com>
2017-08-17 15:57:42 +0800

    Add files via upload

--- server/models/car.ts
+++ server/models/car.ts
@@ -1,1 +1,10 @@
+import * as mongoose from 'mongoose';
 
+const carSchema = new mongoose.Schema({
+  name: String,
+  traffic: String
+});
+
+const Car = mongoose.model('Car', carSchema);
+
+export default Car;
